fe7ab3be750568406b35aaac83d08acec75661fb
Misha Panyushkin <misha.panyushkin@gmail.com>
2016-07-12 02:01:40 +0300

    Content: added contacts.

--- web/public/scripts/UI/Home/Info/Info.react.js
+++ web/public/scripts/UI/Home/Info/Info.react.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import _b from 'bem-cn'
 
+import CustomLink from 'UI/Pure/CustomLink/CustomLink.react'
+
 export default class Info extends Component {
     constructor (props) {
         super(props);
@@ -13,7 +15,7 @@ export default class Info extends Component {
     render () {
         return (
             <article
-                className={ this.block() }>
+                className={ this.block().mix('selectable') }>
                 <hgroup className={ this.block('header') }>
                     <img className="logo" src="/materials/images/redenergy@2x.png"/>
                     <h1 className="cHeader title">aloudly</h1>
@@ -22,6 +24,11 @@ export default class Info extends Component {
                 </hgroup>
                 <p className={ this.block('description').mix("cParagraph") }>
                     We bring advanced technologies to the academic world. On the way to Create an unique technique for distinguishing and eliminating human speech defects and mispronunciations. It's a startup and we are moving forward every day.
+                </p>
+                <p className={ this.block('contact').mix("cParagraph") }>
+                    Would like to participate or check out the first fresh beta version? 
+                    <CustomLink link="mailto:hi@aloudly.education" className="emphasized email_link" faClassName="">Contact us</CustomLink>
+                    &nbsp;in any case!
                 </p>
             </article>
         )
